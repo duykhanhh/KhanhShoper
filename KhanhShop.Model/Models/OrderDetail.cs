d75538f50289efab8930078854b553f8e60d607e
vuduykhanh1999 <vuduykhanh1999@gmail.com>
2021-03-19 11:01:18 +0700

    add domain entity

---
+++ KhanhShop.Model/Models/OrderDetail.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KhanhShop.Model.Models
+{
+    [Table("OrderDetails")]
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderId { set; get; }
+
+        [Key]
+        public int ProductId { set; get; }
+
+        public int Quantity { set; get; }
+
+        [ForeignKey("OrderId")]
+        public virtual Order Order { set; get; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { set; get; }
+    }
+}
